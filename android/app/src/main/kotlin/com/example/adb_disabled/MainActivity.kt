7134e52d54d35c3e917994557ba4ab26c580bdcb
Mouhib777 <mouhibsahbani@gmail.com>
2023-07-04 22:11:28 +0000

    c

--- android/app/src/main/kotlin/com/example/adb_disabled/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/adb_disabled/MainActivity.kt
@@ -1,6 +1,32 @@
 package com.example.adb_disabled
 
-import io.flutter.embedding.android.FlutterActivity
+import android.os.Build;
+import android.provider.Settings;
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.MethodChannel;
+
 
 class MainActivity: FlutterActivity() {
+    private val adbChannel = "adb_disable_app/channel"
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, adbChannel).setMethodCallHandler { call, result ->
+            if (call.method == "disableADB") {
+                disableADB()
+                result.success(null)
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+
+    private fun disableADB() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            Settings.Global.putInt(contentResolver, Settings.Global.ADB_ENABLED, 0)
+        } else {
+            Settings.Secure.putInt(contentResolver, Settings.Secure.ADB_ENABLED, 0)
+        }
+    }
 }
